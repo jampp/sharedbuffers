d18b86a8eea64d4ed01c71d2dc279b1dfc837127
llogiudice <luciano.lo.giudice@jampp.com>
2018-10-03 14:50:24 -0300

    Test proxied_dict with heterogeneous keys

--- tests/mapped_struct.py
+++ tests/mapped_struct.py
@@ -1629,7 +1629,8 @@ class ProxiedDictPackingTest(unittest.TestCase, CollectionPackingTestHelpers, Di
         {},
         {'a': 'a2', 'b': 'b2', 'c': 'c2'},
         {1: 10, 2: 20, 3: 30},
-        {'a': frozenset(), 'b': (1, 2), 'c': 1.0, 'd': [1, 2], 'e': dict(a=1)},
+        {'a': frozenset(), 'b': (1, 2), 'c': 1.0, 'd': [1, 2], 'e': dict(a=1) },
+        {1: 42, 'a1_@!': 69, 3.5: 'uhhhh'}
     ]
 
 class MappedDatetimePackingTest(unittest.TestCase):
