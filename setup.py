c85e2e672a5bd49fbbca02542d43e20e8037c6ff
Claudio Freire <klaussfreire@gmail.com>
2022-01-14 10:24:51 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ else:
     extra['ext_modules'] = lazy_modules()
     extra['setup_requires'] = setup_requires
 
-VERSION = "0.9.1"
+VERSION = "0.9.2"
 
 version_path = os.path.join(os.path.dirname(__file__), 'sharedbuffers', '_version.py')
 if not os.path.exists(version_path):
