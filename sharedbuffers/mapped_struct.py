1c3e7476984a608aa9e899df632d1481637c84bc
Christian Pedersen <christian.pedersen@jampp.com>
2022-06-09 14:06:35 -0300

    BTS-619: rm pure python stuff

--- sharedbuffers/mapped_struct.py
+++ sharedbuffers/mapped_struct.py
@@ -3352,10 +3352,9 @@ class BufferProxyObject(object):
 
     def __del__(self):
         # For python 3.4+, in earlier versions it will not be called and we'll depend on dealloc
-        if cython.compiled:
-            if self.pybuf.buf != cython.NULL:
-                PyBuffer_Release(cython.address(self.pybuf))  # lint:ok
-                self.pybuf.buf = cython.NULL
+        if self.pybuf.buf != cython.NULL:
+            PyBuffer_Release(cython.address(self.pybuf))  # lint:ok
+            self.pybuf.buf = cython.NULL
 
 @cython.cclass
 class BaseBufferProxyProperty(object):
