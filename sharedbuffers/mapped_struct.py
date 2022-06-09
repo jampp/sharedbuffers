357936ed65e0ce6a477dc4ee84eecde4346e2ad3
Christian Pedersen <christian.pedersen@jampp.com>
2022-06-09 14:25:28 -0300

    BTS-619: rm pure python stuff

--- sharedbuffers/mapped_struct.py
+++ sharedbuffers/mapped_struct.py
@@ -3662,16 +3662,14 @@ class ULongBufferProxyProperty(BaseBufferProxyProperty):
             return self
         elif obj.none_bitmap & self.mask:
             return None
-        if cython.compiled:
-            assert (obj.offs + self.offs + cython.sizeof(cython.ulong)) <= obj.pybuf.len  # lint:ok
-            rv = cython.cast(cython.p_ulonglong,
-                cython.cast(cython.p_uchar, obj.pybuf.buf) + obj.offs + self.offs)[0]  # lint:ok
-            if rv <= cython.cast(cython.ulonglong, 0x7FFFFFFFFFFFFFFF):
-                return cython.cast(cython.longlong, rv)
-            else:
-                return rv
+
+        assert (obj.offs + self.offs + cython.sizeof(cython.ulong)) <= obj.pybuf.len  # lint:ok
+        rv = cython.cast(cython.p_ulonglong,
+            cython.cast(cython.p_uchar, obj.pybuf.buf) + obj.offs + self.offs)[0]  # lint:ok
+        if rv <= cython.cast(cython.ulonglong, 0x7FFFFFFFFFFFFFFF):
+            return cython.cast(cython.longlong, rv)
         else:
-            return struct.unpack_from('Q', obj.buf, obj.offs + self.offs)[0]
+            return rv
 
     @cython.locals(obj = BufferProxyObject, elem = cython.ulonglong)
     def __set__(self, obj, elem):
