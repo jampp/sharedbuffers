97811ea302793a527c646e54dd4a5bca612aad5b
Christian Pedersen <christian.pedersen@jampp.com>
2022-06-09 14:24:26 -0300

    BTS-619: rm pure python stuff

--- sharedbuffers/mapped_struct.py
+++ sharedbuffers/mapped_struct.py
@@ -7603,19 +7603,19 @@ class NumericIdMultiMapper(NumericIdMapper):
         if 0 <= startpos < nitems:
             buf = self._buf
             dtype = self._dtype
-            if cython.compiled:
-                if dtype is npuint64:
-                    return _numeric_id_multi_has_gen[cython.ulonglong](
-                        self, 0, startpos, hkey)
-                elif dtype is npuint32:
-                    return _numeric_id_multi_has_gen[cython.uint](
-                        self, 0, startpos, hkey)
-                elif dtype is npuint16:
-                    return _numeric_id_multi_has_gen[cython.ushort](
-                        self, 0, startpos, hkey)
-                elif dtype is npuint8:
-                    return _numeric_id_multi_has_gen[cython.uchar](
-                        self, 0, startpos, hkey)
+
+            if dtype is npuint64:
+                return _numeric_id_multi_has_gen[cython.ulonglong](
+                    self, 0, startpos, hkey)
+            elif dtype is npuint32:
+                return _numeric_id_multi_has_gen[cython.uint](
+                    self, 0, startpos, hkey)
+            elif dtype is npuint16:
+                return _numeric_id_multi_has_gen[cython.ushort](
+                    self, 0, startpos, hkey)
+            elif dtype is npuint8:
+                return _numeric_id_multi_has_gen[cython.uchar](
+                    self, 0, startpos, hkey)
 
             index = self.index
             return startpos < nitems and index[startpos,0] == hkey
