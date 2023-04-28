f684da3f36d043eb0d76814c54a9c32bc5ed20f5
Christian Pedersen <christian.pedersen@jampp.com>
2023-04-28 11:14:01 -0300

    fix fileno

--- sharedbuffers/mapped_struct.py
+++ sharedbuffers/mapped_struct.py
@@ -6913,7 +6913,12 @@ class ObjectIdMapper(_CZipMapBase):
             access = mmap.ACCESS_READ
         else:
             access = mmap.ACCESS_WRITE
-        buf = mmap.mmap(fileobj.fileno(), map_size, flags = mmap.MAP_SHARED, access = access, offset = map_start)
+
+        if hasattr(fileobj, "_file"):
+            fileno = fileobj._file.fileno()
+        else:
+            fileno = fileobj.fileno()
+        buf = mmap.mmap(fileno, map_size, flags = mmap.MAP_SHARED, access = access, offset = map_start)
         rv = cls(buf, offset - map_start)
         rv._file = fileobj
         return rv
