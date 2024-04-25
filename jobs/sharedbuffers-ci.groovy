119e075b8ecf476c8b68b6e92e85b20ed50bb481
mpucciariello <micaela.pucciariello@jampp.com>
2024-04-25 10:00:48 -0300

    PLAT-228: added sharedbuffers ci

---
+++ jobs/sharedbuffers-ci.groovy
@@ -0,0 +1,48 @@
+@Library("jampp-shared-libraries@v2.14.3") _
+
+pipeline {
+    agent {
+        node {
+            label "spot-small"
+        }
+    }
+
+    environment {
+        PYTHONTEST_IMAGE_VERSION = "3.11.0-python3"
+        REQUIRES_SDIST = "true"
+    }
+
+    stages {
+        stage("Run CI") {
+            steps {
+                script {
+                    docker.image("docker.jampp.com/pythontest-image-builder:${PYTHONTEST_IMAGE_VERSION}").inside(
+                        """\
+                        -v ${WORKSPACE}:/src \
+                        -e REQUIRES_SDIST \
+                        """
+                    ) {
+                        sh "/docker-entrypoint.sh pytest_coverage"
+                    }
+                }
+
+                junit "output.xml"
+                cobertura autoUpdateHealth: false,
+                autoUpdateStability: false,
+                coberturaReportFile: "coverage.xml",
+                failNoReports: true,
+                failUnhealthy: false,
+                failUnstable: false,
+                maxNumberOfBuilds: 0,
+                onlyStable: false,
+                sourceEncoding: "ASCII",
+                zoomCoverageChart: false,
+                lineCoverageTargets: '80, 0, 0',
+                methodCoverageTargets: '80, 0, 0',
+                conditionalCoverageTargets: '80, 0, 0'
+
+                archiveArtifacts "dist/sharedbuffers-*.tar.gz"
+            }
+        }
+    }
+}
