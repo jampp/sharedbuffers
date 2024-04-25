2af7a8a43d67a68e256a052f90b5f0d1e82202f3
mpucciariello <micaela.pucciariello@jampp.com>
2024-04-25 10:56:36 -0300

    PLAT-228: fixed python version

--- jobs/sharedbuffers-ci.groovy
+++ jobs/sharedbuffers-ci.groovy
@@ -11,12 +11,23 @@ pipeline {
         PYTHONTEST_IMAGE_VERSION = "3.5.0"
         REQUIRES_SDIST = "true"
     }
+    when {
+        expression{
+            return hasChangesIn.run("sharedbuffers")
+        }
+    }
+    axes {
+        axis {
+            name 'PYTHON_VERSION'
+            values 'python3.11'
+        }
+    }
 
     stages {
         stage("Run CI") {
             steps {
                 script {
-                    docker.image("docker.jampp.com/pythontest-image-builder:${PYTHONTEST_IMAGE_VERSION}").inside(
+                    docker.image("docker.jampp.com/libcore:${PYTHONTEST_IMAGE_VERSION}-${PYTHON_VERSION}").inside(
                         """\
                         -v ${WORKSPACE}:/src \
                         -e REQUIRES_SDIST \
